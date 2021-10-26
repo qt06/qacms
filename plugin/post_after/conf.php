dc91c053bb574d96ace63820e95d292be93d90f5
qt06 <qt06.com@139.com>
2021-10-26 18:12:35 +0800

    new plugin post_after

---
+++ plugin/post_after/conf.php
@@ -0,0 +1,10 @@
+<?php
+
+return 	array (
+	'name' => '发帖后通知qq群',		// 插件名
+	'brief' => '发布主题和回复后通知qq群。',
+	'version' => '1.0.0',		// 插件版本
+	'bbs_version' => '2.1.0',		// 插件支持的 Xiuno BBS 版本
+	'cateid' => 4,
+);
+?>
